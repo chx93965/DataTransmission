67ed4a584402aad904a8003771293e6ba216c65e
changh28 <71030485+changh28@users.noreply.github.com>
2024-01-13 22:11:34 -0500

    transmitter & receiver

--- python/client/StatsReceiver.py
+++ python/client/StatsReceiver.py
@@ -1,33 +1,62 @@
 import logging
-from SX127x.LoRa import *
-from SX127x.board_config import BOARD
+import socket
+import time
 
-logger2 = logging.getLogger("LoRaRxImpl")
-logger2.setLevel(logging.DEBUG)
+# from SX127x.LoRa import *
+# from SX127x.board_config import BOARD
 
-# LoRa parameters
-frequency = 915e6   # 915 MHz
-verbose = True
+logger = logging.getLogger("StatsReceiver")
+logger.setLevel(logging.DEBUG)
+# logger2 = logging.getLogger("LoRaRxImpl")
+# logger2.setLevel(logging.DEBUG)
+
+# # LoRa parameters
+# frequency = 915e6   # 915 MHz
+# verbose = True
+# TODO: Move to config file
+host = '0.0.0.0' # listen on all interfaces
+port = 8080
 
 class StatsReceiver:
 
         def __init__(self):
-            self.__lora = LoRaRxImpl(verbose)
+            # self.__lora = LoRaRxImpl(verbose)
+            self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.__socket.bind((host, port))
+            # start listening for connections
+            self.__socket.listen(1)
+            logger.info("Listening on " + host + ":" + str(port))
 
         def receive(self):
+            while True:
+                client, address = self.__socket.accept()
+                logger.info("Connection from " + str(address))
+                request = client.recv(1024)
+                stats = request.decode('utf-8')
+                logger.info("Received: " + stats)
+                self.store_stats(stats)
+                client.close()
+
+        def store_stats(self, stats):
             pass
 
-class LoRaRxImpl(LoRa):
+        def get_socket(self):
+            return self.__socket
 
-    def __init__(self, verbose):
-        super(LoRaRxImpl, self).__init__(verbose)
-        self.set_mode(MODE.SLEEP)
+        def set_socket(self, socket):
+            self.__socket = socket
 
-    def on_rx_done(self):
-        logger2.info("RxDone")
-        self.set_mode(MODE.SLEEP)
-        self.reset_ptr_rx()
-        self.set_mode(MODE.RXCONT)
-
-    def on_rx_timeout(self):
-        logger2.info("RxTimeout")
+# class LoRaRxImpl(LoRa):
+#
+#     def __init__(self, verbose):
+#         super(LoRaRxImpl, self).__init__(verbose)
+#         self.set_mode(MODE.SLEEP)
+#
+#     def on_rx_done(self):
+#         logger2.info("RxDone")
+#         self.set_mode(MODE.SLEEP)
+#         self.reset_ptr_rx()
+#         self.set_mode(MODE.RXCONT)
+#
+#     def on_rx_timeout(self):
+#         logger2.info("RxTimeout")
--- python/client/StatsTransmitter.py
+++ python/client/StatsTransmitter.py
@@ -1,33 +1,50 @@
 import logging
+import socket
+
 from SX127x.LoRa import *
 
 from service.StatsHandler import StatsHandler
 
-logger1 = logging.getLogger("StatsTramsmitter")
-logger1.setLevel(logging.DEBUG)
-logger2 = logging.getLogger("LoRaTxImpl")
-logger2.setLevel(logging.DEBUG)
+logger = logging.getLogger("StatsTramsmitter")
+logger.setLevel(logging.DEBUG)
+# logger2 = logging.getLogger("LoRaTxImpl")
+# logger2.setLevel(logging.DEBUG)
 
-# LoRa parameters
-frequency = 915e6   # 915 MHz
-verbose = True
+# # LoRa parameters
+# frequency = 915e6   # 915 MHz
+# verbose = True
+# TODO: Move to config file
+server = '127.0.0.1' # server IP
+port = 8080
 
 class StatsTransmitter:
 
     def __init__(self):
+        # self.__lora = LoRaTxImpl(verbose)
         self.__stats_handler = StatsHandler()
-        self.__lora = LoRaTxImpl(verbose)
+        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # connect to server
+        self.__socket.connect((server, port))
 
-    def transmit(self):
-        pass
+    def transmit(self, id):
+        stats = self.__stats_handler.pop_stats(id)
+        logger.info("Transmitting: " + str(stats))
+        self.__socket.sendall(str(stats).encode('utf-8'))
+        self.__socket.close()
+
+    def get_socket(self):
+        return self.__socket
+
+    def set_socket(self, socket):
+        self.__socket = socket
 
 
-class LoRaTxImpl(LoRa):
-
-    def __init__(self, verbose):
-        super(LoRaTxImpl, self).__init__(verbose)
-        self.set_mode(MODE.TX)
-
-    def on_tx_done(self):
-        logger2.info("TxDone")
-        self.set_mode(MODE.SLEEP)
+# class LoRaTxImpl(LoRa):
+#
+#     def __init__(self, verbose):
+#         super(LoRaTxImpl, self).__init__(verbose)
+#         self.set_mode(MODE.TX)
+#
+#     def on_tx_done(self):
+#         logger2.info("TxDone")
+#         self.set_mode(MODE.SLEEP)
